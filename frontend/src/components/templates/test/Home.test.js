937681ffd132ebb166a8000472336b33fa880f3d
joaochioda <joao.chioda@dextra-sw.com>
2021-08-22 15:15:33 -0300

    finalizando test front

--- frontend/src/components/templates/test/Home.test.js
+++ frontend/src/components/templates/test/Home.test.js
@@ -1,7 +1,11 @@
 /* eslint-disable no-undef */
 import React from "react";
 import { render, waitFor, screen } from "@testing-library/react";
+import ReactRouter from "react-router";
+
 import Home from "../Home";
+import Book from "../Book";
+
 import Theme from "../../../styles/theme";
 import axios from "axios";
 
@@ -48,5 +52,46 @@ describe("Home", () => {
     expect(element[0].textContent).toEqual("TAG4.39GOODREADS3.99");
     expect(element[1].textContent).toEqual("TAG3.83GOODREADS3.90");
     expect(element[2].textContent).toEqual("TAG4.30GOODREADS4.18");
+  });
+});
+
+describe("Book", () => {
+  beforeEach(async () => {
+    axios.get.mockImplementation((url) => {
+      switch (url) {
+        case "/tag/book/1WHexRrC5v":
+          return Promise.resolve({ data: dataTag[0] });
+        case "/goodreads/books/9788525063557":
+          return Promise.resolve({ data: dataGoodRead[0] });
+        default:
+          return Promise.reject(new Error("not found"));
+      }
+    });
+    jest
+      .spyOn(ReactRouter, "useParams")
+      .mockReturnValue({ bookId: dataTag[0].objectId });
+
+    await waitFor(async () =>
+      render(
+        <Theme>
+          <Book />
+        </Theme>
+      )
+    );
+  });
+
+  test("Should render title", async () => {
+    const title = screen.getByText("O Xará");
+    expect(title).toBeTruthy();
+  });
+
+  test("Should render img", async () => {
+    const img = screen.getByRole("img");
+    expect(img.src).toEqual(dataTag[0].cover.url);
+  });
+
+  test("Should render pages", async () => {
+    const pages = screen.getByText("336 páginas");
+    expect(pages).toBeTruthy();
   });
 });
